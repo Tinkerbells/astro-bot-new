44cffaefdfb25cebf0cda463e34823d775449e1e
tinkerbells <daniel.kasatkin.work@gmail.com>
2025-10-14 20:45:40 +0300

    feat: добавить enum MenuId для идентификаторов меню
    - заменить константы на enum MenuId
    - улучшить типобезопасность при работе с меню
    - упростить импорты и использование идентификаторов меню

---
+++ src/bot/shared/menus/menu-ids.ts
@@ -0,0 +1,13 @@
+/**
+ * Enum ID всех меню
+ * Вынесены в отдельный файл для избежания циклических зависимостей
+ */
+
+export enum MenuId {
+  Profile = 'profile-menu',
+  CancelConversation = 'cancel-menu',
+  Ascendants = 'ascendants-menu',
+  NatalCharts = 'natal-charts-menu',
+  Compatibilities = 'compatibilities-menu',
+  CompatibilitiesList = 'compatibilities-list-menu',
+}
